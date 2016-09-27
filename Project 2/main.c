e92cb94f6c93d2e7d5275f3bf59caa902d3222ac
fords <zxw6805@rit.edu>
2016-09-27 18:37:19 -0500

    get commands from terminal

--- Project 2/main.c
+++ Project 2/main.c
@@ -25,18 +25,18 @@ void TIM5_IRQHandler (void)
 	{
 	if(test==0)
 		{
-		Red_LED_Toggle();		//Test purpose
-		Green_LED_Toggle();		//Test purpose
+		/*Red_LED_Toggle();		//Test purpose
+		Green_LED_Toggle();		//Test purpose*/
 		test=1;
 		}
 	else if(test==1)
 		{
-		Green_LED_Toggle(); //Test purpose
-		Red_LED_Toggle();		//Test purpose
+		/*Green_LED_Toggle(); //Test purpose
+		Red_LED_Toggle();		//Test purpose*/
 		test=0;
 		}
     TIM5->SR &= ~(1<<0);        // Clear Interrupt Flag
-
+	runsnipp();
  }
 }
 
@@ -51,7 +51,36 @@ int main(void){
 	timer_wait();							// Initialize Timer Interrupt
 	Green_LED_Off();					// Reset LED before begin
 	Red_LED_Off();						// Reset LED before begin
-  TIM5_IRQHandler();
 
+	while (1)
+		{
+			/***Enter Command1 for controlling Servo 1***/
+			USART_Write(USART2, (uint8_t *)command1, strlen(command1));
+			rx_cmd1= USART_Read(USART2);
+			m = sprintf((char *)buffer, "%c", rx_cmd1);
+			USART_Write(USART2, buffer, m);
+
+			/***Enter Command2 for controlling Servo 2***/
+			USART_Write(USART2, (uint8_t *)command2, strlen(command2));
+			rx_cmd2= USART_Read(USART2);
+			n = sprintf((char *)buffer, "%c", rx_cmd2);
+			USART_Write(USART2, buffer, n);
+
+			/***Press Enter to Start/Alter servo operation***/
+			USART_Write(USART2, (uint8_t *)command3, strlen(command3));
+			rx_cmd3= USART_Read(USART2);
+
+			user_command(rx_cmd1,rx_cmd2,rx_cmd3);
+		}
 
 }
+
+	/***User defined function called to control servo1 and servo2***/
+	void user_command(uint8_t input1, uint8_t input2, uint8_t input3)
+	{
+
+		if ( input3 == 0xd) 									//Check if Enter is presssed
+			{
+        sprintf("enter");
+			}
+  }
